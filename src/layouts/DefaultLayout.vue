fa0496e65a3123c2760e3a403e7d34b3687dabc0
tctienn <mainichichimvm@gmail.com>
2024-05-01 01:33:27 +0700

    s

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,4 +1,4 @@
-<script setup>
+<!-- <script setup>
 import { CContainer } from '@coreui/vue'
 import AppFooter from '@/components/AppFooter.vue'
 import AppHeader from '@/components/AppHeader.vue'
@@ -18,4 +18,45 @@ import AppSidebar from '@/components/AppSidebar.vue'
       <AppFooter />
     </div>
   </div>
+</template> -->
+
+<template>
+  <Login v-if="!user" />
+  <div v-else>
+    <AppSidebar />
+    <div class="wrapper d-flex flex-column min-vh-100">
+      <AppHeader />
+      <div class="body flex-grow-1">
+        <CContainer class="px-4" lg>
+          <router-view />
+        </CContainer>
+      </div>
+      <AppFooter />
+    </div>
+  </div>
 </template>
+<script>
+import { CContainer } from '@coreui/vue'
+import AppFooter from '@/components/AppFooter.vue'
+import AppHeader from '@/components/AppHeader.vue'
+import AppSidebar from '@/components/AppSidebar.vue'
+import Login from '../views/login.vue'
+import { getCookie } from '../api/CookieFuntion'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    AppFooter,
+    AppHeader,
+    AppSidebar,
+    CContainer,
+    Login,
+  },
+  setup() {
+    const user = getCookie('login_token_qlsp')
+    return {
+      user,
+    }
+  },
+}
+</script>
